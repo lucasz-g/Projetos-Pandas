3a9b378e50db44288d0fdf40072fe70508f51e23
Evosystem 02 <evosystem02.ti@wheatonbrasil.com.br>
2025-02-18 13:52:38 -0300

    Iniciando com streamlit

--- Streamlit/Primeiro-App/app.py
+++ Streamlit/Primeiro-App/app.py
@@ -1,6 +1,24 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 st.title('File Reader')
 
-st.file_uploader()
+st.text('Esse é um app que retorna informações sobre gotas cortadas.')
+
+arquivo = st.file_uploader('Faça o upload do seu arquivo.')
+
+if arquivo:
+    st.header('Data Statistics')
+
+    df = pd.read_csv(arquivo, encoding='UTF-8')
+    st.write(df.describe().round(2))
+
+    st.header('Data Header')
+    st.write(df.head())
+
+    fig = px.scatter(df, x='Comprimento médio (mm)', y='Diâmetro',
+                     labels={'x': 'Comprimento médio', 'y': 'Diâmetro'},
+                     title='Dispersão entre Comprimento médio e Diâmetro')
+
+    st.plotly_chart(fig)
